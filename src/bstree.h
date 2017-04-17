afbb3e4ece75d957c6bbccdff76225f52b0948ca
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-17 12:51:44 +0700

    Add header bstree

---
+++ src/bstree.h
@@ -0,0 +1,18 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+
+//typedef struct tree tree;
+struct bstree{
+    char *value;
+    int key;
+    struct bstree *left;
+    struct bstree *right;
+	struct bstree *parent;
+};
+
+struct bstree *bstree_create(char *value, int key);
+void bstree_add(struct bstree *tree, char *value, int key);
+struct bstree *bstree_lookup(struct bstree *tree, char *value);
+struct bstree *bstree_min(struct bstree *tree);
+struct bstree *bstree_max(struct bstree *tree);
+#endif
