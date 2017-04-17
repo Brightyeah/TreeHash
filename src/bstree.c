c5138515e504b5ff589215bb63811410fd623419
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-04-17 13:30:37 +0700

    Adding a new bstree

--- src/bstree.c
+++ src/bstree.c
@@ -12,3 +12,27 @@ struct bstree *bstree_create(char *value, int key)
     }
     return create;
 }
+
+void bstree_add(struct bstree *tree, char *value, int key)
+{
+    struct bstree *add;
+    if (tree == NULL) {
+        return;
+    }
+    while (tree != NULL) {
+        tree->parent = tree;
+        if (key < tree->key) {
+            tree = tree->left;
+        } else if (key > tree->key) {
+            tree = tree->right;
+        } else {
+            return;
+        }
+    }
+    add = bstree_create(value, key);
+    if (key < tree->parent->key) {
+        tree->left = add;
+    } else if (key > tree->parent->key) {
+        tree->right = add;
+    }
+}
